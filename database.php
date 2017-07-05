efce114e6611f57d328b490d76bb155bedd20249
Anna Litvinenko <al224@afsconnect1.njit.edu>
2017-07-04 21:16:27 -0400

    new change

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-    $username = 'mgs_user';
-    $password = 'pa55word';
+    $dsn = 'mysql:dbname=al224;host=sql1.njit.edu';
+    $username = 'al224';
+    $password = 'wSt5Tjc0';
 
     try {
         $db = new PDO($dsn, $username, $password);
